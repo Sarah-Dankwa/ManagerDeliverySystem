23c5e62141c5b3f69312f00e4c8009ec0a1a6980
Admin <Admin@STUDENT06>
2022-07-21 12:07:53 +0100

    Added Driver infrastructure

---
+++ src/main/java/com/qa/domain/Driver.java
@@ -0,0 +1,63 @@
+package com.qa.domain;
+
+public class Driver {
+
+private int driverID;
+private String firstName;
+private String surname;
+
+public Driver(int driverID, String firstName, String surname) {
+	this.driverID = driverID;
+	this.firstName = firstName;
+	this.surname = surname;
+}
+
+public Driver(String firstName, String surname) {
+	this.firstName = firstName;
+	this.surname = surname;
+}
+
+
+
+@Override
+public String toString() {
+	return "Driver [driverID=" + driverID + ", firstName=" + firstName + ", surname=" + surname + "]";
+}
+
+
+public int getDriverID() {
+	return driverID;
+}
+
+
+
+public void setDriverID(int driverID) {
+	this.driverID = driverID;
+}
+
+
+
+public String getFirstName() {
+	return firstName;
+}
+
+
+
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+
+
+
+public String getSurname() {
+	return surname;
+}
+
+
+
+public void setSurname(String surname) {
+	this.surname = surname;
+}
+
+	
+}
